dcbc49db99a88374184b9bd5de49391dae925eba
Ignacio Iglesias <iiglesiascampoy@gmail.com>
2020-02-21 22:04:56 -0700

    Add native base

--- src/Components/organisms/PatientsList.js
+++ src/Components/organisms/PatientsList.js
@@ -1,56 +1,40 @@
 import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
-import {Avatar, Title, IconButton} from 'react-native-paper';
-
+import {
+  Container,
+  Content,
+  List,
+  ListItem,
+  Left,
+  Body,
+  Right,
+  Thumbnail,
+  Text,
+} from 'native-base';
+import {IconButton} from 'react-native-paper';
 export default class PatientsList extends Component {
   render() {
     return (
-      <View>
-        <View style={styles.container}>
-          <View style={styles.principalView}>
-            <View style={styles.content}>
-              <Avatar.Image
-                size={60}
-                source={require('../../img/usuario.png')}
-                style={{backgroundColor: 'white'}}
-              />
-              <View style={styles.contentRight}>
-                <Title>Walter White</Title>
-                <View>
-                  <IconButton
-                    icon="trash-can-outline"
-                    color="red"
-                    onPress={() => console.log('Pressed')}
-                    style={{marginLeft: 50}}
-                  />
-                </View>
-              </View>
-            </View>
-          </View>
-        </View>
-      </View>
+      <Container>
+        <Content>
+          <List>
+            <ListItem avatar icon>
+              <Left>
+                <Thumbnail source={require('../../img/usuario.png')} />
+              </Left>
+              <Body>
+                <Text>Walter White</Text>
+              </Body>
+              <Right>
+                <IconButton
+                  icon="trash-can-outline"
+                  color="red"
+                  onPress={() => console.log('Pressed')}
+                />
+              </Right>
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
     );
   }
 }
-const styles = StyleSheet.create({
-  container: {
-    padding: 20,
-  },
-  principalView: {
-    display: 'flex',
-    flexDirection: 'row',
-    borderWidth: 3,
-    borderRadius: 5,
-  },
-  content: {
-    display: 'flex',
-    flexDirection: 'row',
-    margin: 10,
-  },
-  contentRight: {
-    display: 'flex',
-    flexDirection: 'row',
-    marginLeft: 40,
-    paddingTop: 10,
-  },
-});
